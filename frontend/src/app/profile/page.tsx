e69243ed7e6ef805a5ff3489b0b59de91555f9ac
Deterjen <samuel.ysusanto@gmail.com>
2025-04-01 16:52:14 +0800

    Change some of the API handling

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -62,7 +62,7 @@ export default function ProfilePage() {
       }
 
       await authService.updateUser(username, {
-        email: data.email,
+        username: data.email, 
         current_password: data.currentPassword,
         new_password: data.newPassword,
       })
